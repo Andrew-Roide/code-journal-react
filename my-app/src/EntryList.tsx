50ffaa60f31457357e5b58d1b018bc14af3c6f71
Andrew Roide <andrew.roide@hotmail.com>
2024-01-19 04:40:00 +0000

    Completed issue-2

--- my-app/src/EntryList.tsx
+++ my-app/src/EntryList.tsx
@@ -1,3 +1,48 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import { readEntries } from './data';
+
 export default function EntryList() {
-  return <></>;
+  return (
+    <div className="container" data-view="entries">
+      <div className="row">
+        <div className="column-full d-flex justify-between align-center">
+          <h1>Entries</h1>
+          <h3>
+            <button id="formLink" className="white-text form-link">
+              NEW
+            </button>
+          </h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column-full">
+          <ul className="entry-ul">
+            {readEntries().map((entry) => (
+              <li key={entry.entryId} data-entry-id={entry.entryId}>
+                <div className="row">
+                  <div className="column-half">
+                    <img
+                      className="input-b-radius form-image"
+                      src={entry.photoUrl}
+                      alt={`Image of ${entry.title}`}
+                    />
+                  </div>
+                  <div className="column-half">
+                    <div className="row">
+                      <div className="column-full d-flex justify-between">
+                        <h3>{entry.title}</h3>
+                        <FontAwesomeIcon icon={faPencil} cursor="pointer" />
+                      </div>
+                    </div>
+                    <p>{entry.notes}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
